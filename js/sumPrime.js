2b01d8ab2c805b4855a43b666def55e85fbe5967
CH623 <160983250+CH623@users.noreply.github.com>
2024-06-14 18:12:00 +0800

    Create sumPrime.js

---
+++ js/sumPrime.js
@@ -0,0 +1,16 @@
+function sumPrime(n){
+    let sum=0
+
+    for (let i=2; i<=n; i++){
+        for (let j=2; j<=i; j++){
+            if (i==j){
+                sum+=j
+            }
+            else if(i%j==0) break
+
+        }
+    }
+    return sum
+}
+
+console.log("sumPrime(10)=",sumPrime(10))
